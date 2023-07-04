bb478389fa059369770ecdb30507a5283bd175ed
Isheta <102668226+Isheta20@users.noreply.github.com>
2023-07-04 18:44:28 +0530

    Add files via upload

--- contracts/EthAvaxAssessment.sol
+++ contracts/EthAvaxAssessment.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
 
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract EthAvaxAssessment{
+
+    uint public totalSmiles;
+    constructor(uint initSmiles) {
+        totalSmiles = initSmiles;
+        console.log("let's see some smiles!");
+    }
+
+    function recieveSmile() public {
+        totalSmiles += 1;
+        console.log("%s has smiled!", msg.sender);
+    }
+
+    function sendSmile() public {
+        totalSmiles -= 1;
+    }
+
+    function getTotalSmiles() public view returns (uint256) {
+        console.log("We have %d total smiles!", totalSmiles);
+        return totalSmiles;
+    }
+
+}
