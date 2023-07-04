8ca6869774edc36d13a9dccd1bd81720b1042065
Isheta <102668226+Isheta20@users.noreply.github.com>
2023-07-04 18:58:26 +0530

    Add files via upload

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,18 @@
+const hre = require("hardhat");
 
+async function main() {
+  const initSmiles = 10;
+  const EthAssessment = await hre.ethers.getContractFactory("EthAvaxAssessment");
+  const ethassessment = await EthAssessment.deploy(initSmiles);
+  await ethassessment.waitForDeployment();
+
+  const address = await ethassessment.getAddress();
+  console.log("EthAvaxAssessment is deployed to: ",address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
