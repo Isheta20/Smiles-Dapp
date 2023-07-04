2eff8575959d00d567ce103b3888c467c8bb7e59
Isheta <102668226+Isheta20@users.noreply.github.com>
2023-07-04 18:59:06 +0530

    deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,8 +10,6 @@ async function main() {
   console.log("EthAvaxAssessment is deployed to: ",address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
